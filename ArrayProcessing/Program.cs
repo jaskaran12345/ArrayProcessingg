2f0fd2393ce55ab409969ee25d4f09b318d5a627
visualstudio <visualstudio@230-PC19>
2019-04-18 15:54:37 -0400

    Assgn

--- ArrayProcessing/Program.cs
+++ ArrayProcessing/Program.cs
@@ -6,7 +6,26 @@ namespace ArrayProcessing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] arr = { 17, 11, 9, 3, 4, 11, 6, 11, 8, 7 };
+            int arr_size = arr.Length;
+
+            printRepeating(arr, arr_size);
+        }
+        static void printRepeating(int[] arr, int size)
+        {
+            int i, j;
+
+            Console.WriteLine("Repeated Elements are :");
+            for (i = 0; i < size; i++)
+            {
+                for (j = i + 1; j < size; j++)
+                {
+                    if (arr[i] == arr[j])
+                        Console.WriteLine(arr[i] + " ");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
